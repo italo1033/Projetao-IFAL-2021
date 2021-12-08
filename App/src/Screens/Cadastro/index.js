55c824ba3a0fd2244bbb3f2c02fce122eead0a2c
italogammy1033@gmail.com <ialo1033>
2021-12-08 02:16:05 -0300

    Adicionando Nome

--- App/src/Screens/Cadastro/index.js
+++ App/src/Screens/Cadastro/index.js
@@ -23,13 +23,17 @@ export function Cadastro({navigation}) {
   const [cpf, setCpf] = useState(null)
   const [errorCpf, setErrorCpf] = useState(null)
 
+  //Nome
+  const [nome, setNome] = useState(null)
+  const [errorNome, setErrorNome] = useState(null)
+
 
 
 
   const validar = () => {
     let error = false
 
-
+    // Validando Contato
     setValidationContato(null)
     const regexContato = /^\(\d{2}\) \d{4,5}-\d{4}$/gi;
     if(!regexContato.test(contato)) {
@@ -37,7 +41,7 @@ export function Cadastro({navigation}) {
       error = true
     }
 
-
+    // Validando Email
     setErrorEmail(null)
     const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!regexEmail.test(String(email).toLowerCase())){
@@ -45,11 +49,19 @@ export function Cadastro({navigation}) {
       error = true
     }
 
-
+  // Validando CPF
     setErrorCpf(null)
     const regexCPF =/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/
-    if (!regexCPF.test(String(cpf).toLowerCase())){
+    if (!regexCPF.test(cpf)){
       setErrorCpf("Preencha seu CPF corretamente")
+      error = true
+    }
+    
+
+    // validando Nome
+    setErrorNome(null)
+    if (nome == "") {
+      setErrorNome("Por favor preencha todos os campos")
       error = true
     }
 
@@ -61,64 +73,74 @@ export function Cadastro({navigation}) {
       console.log("Salvou")
     }
   }
+
+
   return (
     <View style={styles.isBackgroundGeneral}>
+
+      {/* Italo */}
+      <Text>Nome:</Text>
+      <Input
+          placeholder="Maria dos Santos" 
+          onChangeText={value => setNome(value)}
+          returnKeyType="done" 
+          errorMessage={errorNome}
+      />
+
+      {/* Italo */}
+      <Text>CPF:</Text>
+      <Input 
+          keyboardType="number-pad" 
+          placeholder="142.832.344-24" 
+          onChangeText={value => setCpf(value)}
+          returnKeyType="done" 
+          errorMessage={errorCpf}
+      />
 
       {/*Lara Fernanda*/}
       <Text>Contato:</Text>
       <Input 
-      keyboardType="phone-pad" 
-      placeholder="(82)99999-9999" 
-      onChangeText={value => {
-        setContato(value)
-        setValidationContato(null)
-      }}
-      returnKeyType="done" 
-      errorMessage={validationContato}/>  
+            keyboardType="phone-pad" 
+            placeholder="(82)99999-9999" 
+            onChangeText={value => {
+              setContato(value)
+              setValidationContato(null)
+            }}
+            returnKeyType="done" 
+            errorMessage={validationContato}
+      /> 
+
+      {/*Kenysson*/}
+      <Text>Email:</Text>
+      <Input
+            placeholder="receitas12@gmail.com"
+            onChangeText={value => {
+                setEmail(value)
+                setErrorEmail(null)
+            }}
+            keyboardType="email-address"
+            errorMessage={errorEmail}        
+        />
       
+      {/*Kenysson*/}
+      <Text>sexo:</Text>
+      <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
+            <View>
+                  <Text>Masculino</Text>
+                  <RadioButton value="first" />
+            </View>
+            <View>
+                  <Text>Feminino</Text>
+                  <RadioButton value="second" />
+            </View>
+      </RadioButton.Group>
+
       {/*Lara Fernanda*/}
       <Text>Complemento:</Text>
       <Input 
       keyboardType="default" 
       placeholder="Complemento" 
       returnKeyType="done"/>
-
-      <Text>sexo:</Text>
-
-      {/*Kenysson*/}
-      <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-        <View>
-          <Text>Masculino</Text>
-          <RadioButton value="first" />
-        </View>
-        <View>
-          <Text>Feminino</Text>
-          <RadioButton value="second" />
-        </View>
-      </RadioButton.Group>
-
-
-      <Text>Email:</Text>
-      <Input
-        placeholder="receitas12@gmail.com"
-        onChangeText={value => {
-            setEmail(value)
-            setErrorEmail(null)
-        }}
-        keyboardType="email-address"
-        errorMessage={errorEmail}        
-        />
-
-        {/* Italo */}
-
-        <Text>CPF:</Text>
-        <Input 
-          keyboardType="number-pad" 
-          placeholder="142.832.344-24" 
-          onChangeText={value => setCpf(value)}
-          returnKeyType="done" 
-          errorMessage={errorCpf}/>
-
 
     <TouchableOpacity style={styles.button} onPress={() => salvarDados()}>
         <Text style={{color:"#fff"}}> Cadastrar </Text>
