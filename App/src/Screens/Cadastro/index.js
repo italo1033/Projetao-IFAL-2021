6879f891020e2d89cfa8fe0aa38e6e07aeebb2a6
italogammy1033@gmail.com <ialo1033>
2021-12-07 23:54:37 -0300

    add parte da lara

--- App/src/Screens/Cadastro/index.js
+++ App/src/Screens/Cadastro/index.js
@@ -1,10 +1,60 @@
-import * as React from 'react';
+import React, { useState }  from 'react';
 import { styles } from './style.js';
-import { TouchableOpacity, View, Text } from 'react-native';
+import { TouchableOpacity, View } from 'react-native';
+import { Input, Button, Text } from 'react-native-elements';
+
 
 export function Cadastro({navigation}) {
+
+  const [ validationContato, setValidationContato ] = useState(null);
+  const [ contato, setContato ] = useState(null);
+
+  const validar = () => {
+    let error = false
+    setValidationContato(null)
+
+    const regex = /^\(\d{2}\) \d{4,5}-\d{4}$/gi;
+    if(!regex.test(contato)) {
+      setValidationContato("Preencha seu contato")
+      error = true
+    } 
+
+    return !error
+  }
+
+  const salvarDados = () => {
+    if(validar()) {
+      console.log("Salvou")
+    }
+  }
   return (
     <View style={styles.isBackgroundGeneral}>
+
+      {/*Lara Fernanda*/}
+      <Text>Contato:</Text>
+      <Input 
+      keyboardType="phone-pad" 
+      placeholder="Contato" 
+      onChangeText={value => {
+        setContato(value)
+        setValidationContato(null)
+      }}
+      returnKeyType="done" 
+      errorMessage={validationContato}/>  
+      
+      {/*Lara Fernanda*/}
+      <Text>Complemento:</Text>
+      <Input 
+      keyboardType="default" 
+      placeholder="Complemento" 
+      returnKeyType="done"/>
+
+      <Button 
+      title="Cadastrar"
+      size={15}
+
+      onPress={() => salvarDados()}
+      />
 
 
     <TouchableOpacity style={styles.button} onPress={()=>navigation.navigate('Login')}>
